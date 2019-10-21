5236cdcac7f8063425dc3ad6fe603d04cb359691
Judith Cristina <judicriss18@gmail.com>
2019-10-20 20:08:44 -0500

    Updates

---
+++ precache-manifest.370f71f547ae913224683d41d795e838.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14eea69962c4a4868b4690f1cd961a0",
+    "url": "/veterinary-registration/index.html"
+  },
+  {
+    "revision": "5620b020b8ad4555641a",
+    "url": "/veterinary-registration/static/css/main.9acbdca7.chunk.css"
+  },
+  {
+    "revision": "252c36ca7bf1cbb83507",
+    "url": "/veterinary-registration/static/js/2.f1bd69c4.chunk.js"
+  },
+  {
+    "revision": "5620b020b8ad4555641a",
+    "url": "/veterinary-registration/static/js/main.da515b38.chunk.js"
+  },
+  {
+    "revision": "a9a29df805f1d0d7cb94",
+    "url": "/veterinary-registration/static/js/runtime-main.644a0b8a.js"
+  }
+]);
